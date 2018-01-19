d99451778652193fc6659a7ac72a7ddea56abe34
Estevao Cruz <estevaoc@hotmail.com>
2018-01-19 17:00:52 -0200

    done a lot, but not all

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'China in box',
+    address:      'buritis',
+    phone_number:  '(31) 34445566',
+    category:        "chinese"
+  },
+  {
+    name:         'Pizza Sur',
+    address:      'savassi',
+    phone_number:  '31 3456790',
+    category:        "italian"
+  },
+  {
+    name:         'Sushi japa',
+    address:      'sion',
+    phone_number:  '4455-6673',
+    category:        "japanese"
+  },
+  {
+    name:         'Taste Vin',
+    address:      'lourdes',
+    phone_number:  '37485960',
+    category:        "french"
+  },
+  {
+    name:         'WHat',
+    address:      'what street',
+    phone_number:  '212121212',
+    category:        "belgian"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
